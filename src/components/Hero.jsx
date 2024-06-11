236495bd6bf5dbc2b73bbd4202f112fa16036a8b
soma2205 <soma2205da@gmail.com>
2024-06-11 19:33:34 +0530

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,6 +35,7 @@ const Hero = () => {
         </div>
       </div>
 
+      <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
