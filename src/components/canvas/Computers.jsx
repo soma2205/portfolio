6c53b5c963311f9a9d7e3baab920b3bf8ddbb230
soma2205 <soma2205da@gmail.com>
2024-06-11 20:59:03 +0530

    updates

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -60,6 +60,8 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      className="canvas"
+
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
